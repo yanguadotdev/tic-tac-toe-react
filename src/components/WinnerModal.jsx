88446e8cc77de7ba06530d62fa29572ee261284a
Creatlydev <creatlydev@gmail.com>
2024-09-11 11:09:24 -0500

    style: improve winnerModal style

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
-import { RestartIcon } from './Icons.jsx'
 import { Square } from './Square.jsx'
+import { TURNS } from '../constants.js'
+import { OIcon, XIcon, RestartIcon, DrawIcon } from './Icons.jsx'
 
 export function WinnerModal ({ winner, startAgain }) {
   if (winner === null) return null
 
   const winnerText = winner === false ? 'Empate' : 'Ganó'
+
+  const replaceWithIcon = (winner) => winner === TURNS.X
+    ? <XIcon />
+    : winner === TURNS.O
+      ? <OIcon />
+      : <DrawIcon />
 
   return (
     <section className='winner'>
@@ -13,12 +20,12 @@ export function WinnerModal ({ winner, startAgain }) {
         <h2>{winnerText}</h2>
 
         <header className='win'>
-          {winner && <Square>{winner}</Square>}
+          <Square>{replaceWithIcon(winner)}</Square>
         </header>
 
         <footer>
           <button onClick={startAgain}>
-            <RestartIcon />
+            <RestartIcon /> Start again
           </button>
         </footer>
       </div>
