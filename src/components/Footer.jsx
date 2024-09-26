4d53c05333325147e586a232a3155c815c467a59
Creatlydev <creatlydev@gmail.com>
2024-09-25 21:40:51 -0500

    feat: Add custom properties for dynamic 3D button styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { AllowSoundIcon, NotAllowSoundIcon, RepeatIcon } from './Icons'
 export function Footer ({ resetGame, sound, toggleSound }) {
   return (
     <footer className='footer-game'>
-      <button onClick={resetGame}>
+      <button className='Button3D' onClick={resetGame}>
         <RepeatIcon />
       </button>
 
       <span className='label'>1 PLAYER</span>
 
-      <button onClick={() => toggleSound(!sound)}>
+      <button className='Button3D' onClick={() => toggleSound(!sound)}>
         {sound ? <AllowSoundIcon /> : <NotAllowSoundIcon />}
       </button>
     </footer>
--- src/components/HistoryControls.jsx
+++ src/components/HistoryControls.jsx
@@ -3,11 +3,11 @@ import { UndoLeftIcon, UndoRightIcon } from './Icons'
 export function HistoryControls ({ jumpTo }) {
   return (
     <section className='history-controls'>
-      <button onClick={() => jumpTo({ to: 'back' })}>
+      <button className='Button3D' onClick={() => jumpTo({ to: 'back' })}>
         <UndoLeftIcon />
       </button>
 
-      <button onClick={() => jumpTo({ to: 'next' })}>
+      <button className='Button3D' onClick={() => jumpTo({ to: 'next' })}>
         <UndoRightIcon />
       </button>
     </section>
